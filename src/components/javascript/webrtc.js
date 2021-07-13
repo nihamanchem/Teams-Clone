5f5584b931817c3db4305bb1449553628b11d459
nihamanchem <65664278+nihamanchem@users.noreply.github.com>
2021-07-13 12:21:47 +0530

    Update webrtc.js

--- src/components/javascript/webrtc.js
+++ src/components/javascript/webrtc.js
@@ -1,7 +1,7 @@
- import h from './helpers.js';
+ import helper from './helpers.js';
 
  window.addEventListener( 'load', () => {
-     const room = h.getQString( location.href, 'room' );
+     const room = helper.getQString( location.href, 'room' );
      const username = sessionStorage.getItem( 'username' );
  
      if ( !room ) {
@@ -34,7 +34,7 @@
  
  
          socket.on( 'connect', () => {
-             //set socketId
+             // Set socketId
              socketId = socket.io.engine.id;
  
  
@@ -66,12 +66,12 @@
                  if ( data.description.type === 'offer' ) {
                      data.description ? await pc[data.sender].setRemoteDescription( new RTCSessionDescription( data.description ) ) : '';
  
-                     h.getUserFullMedia().then( async ( stream ) => {
+                     helper.getUserFullMedia().then( async ( stream ) => {
                          if ( !document.getElementById( 'local' ).srcObject ) {
-                             h.setLocalStream( stream );
+                            helper.setLocalStream( stream );
                          }
  
-                         //save my stream
+                         // Save my stream
                          myStream = stream;
  
                          stream.getTracks().forEach( ( track ) => {
@@ -95,17 +95,144 @@
  
  
              socket.on( 'chat', ( data ) => {
-                 h.addChat( data, 'remote' );
+                helper.addChat( data, 'remote' );
              } );
          } );
  
+        //Creates offer (description created when a user starts a WebRTC call with other users)
+        function init( createOffer, partnerName ) {
+            pc[partnerName] = new RTCPeerConnection( helper.getIceServer() );
+
+            if ( screen && screen.getTracks().length ) {
+                screen.getTracks().forEach( ( track ) => {
+                    pc[partnerName].addTrack( track, screen );// Should trigger negotiation-needed event
+                } );
+            }
+
+            else if ( myStream ) {
+                myStream.getTracks().forEach( ( track ) => {
+                    pc[partnerName].addTrack( track, myStream ); // Should trigger negotiation-needed event
+                } );
+            }
+
+            else {
+            helper.getUserFullMedia().then( ( stream ) => {
+                    //save my stream
+                    myStream = stream;
+
+                    stream.getTracks().forEach( ( track ) => {
+                        pc[partnerName].addTrack( track, stream ); // Should trigger negotiation-needed event
+                    } );
+
+                    helper.setLocalStream( stream );
+                } ).catch( ( e ) => {
+                    console.error( `stream error: ${ e }` );
+                } );
+            }
+
+            if ( createOffer ) {
+                pc[partnerName].onnegotiationneeded = async () => {
+                    let offer = await pc[partnerName].createOffer();
+
+                    await pc[partnerName].setLocalDescription( offer ); // to set the offer as description of the local end of the connection
+
+                    socket.emit( 'sdp', { description: pc[partnerName].localDescription, to: partnerName, sender: socketId } );
+                };
+            }
+
+            // Sends ice candidate to partnerNames
+            pc[partnerName].onicecandidate = ( { candidate } ) => {
+                socket.emit( 'ice candidates', { candidate: candidate, to: partnerName, sender: socketId } );
+            };
+
+            // Adding the participant
+            pc[partnerName].ontrack = ( e ) => {
+                let str = e.streams[0];
+                if ( document.getElementById( `${ partnerName }-video` ) ) {
+                    document.getElementById( `${ partnerName }-video` ).srcObject = str;
+                }
+
+                else {
+                    //video elem
+
+                    let newVid = document.createElement( 'video' );
+                    newVid.id = `${ partnerName }-video`;
+                    newVid.srcObject = str;
+                    newVid.autoplay = true;
+                    newVid.className = 'remote-video';
+
+                    // Video control elements
+
+                    let controlDiv = document.createElement( 'div' );
+                    controlDiv.className = 'remote-video-controls';
+                    controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
+                        <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
+
+                    // Creates a new div for card
+
+                    let cardDiv = document.createElement( 'div' );
+                    cardDiv.className = 'card card-sm';
+                    cardDiv.id = partnerName;
+                    cardDiv.appendChild( newVid );
+                    cardDiv.appendChild( controlDiv );
+
+                    // Put div in main-section elem
+
+                    document.getElementById( 'videos' ).appendChild( cardDiv );
+
+                    helper.adjustVideoElemSize();
+                }
+            };
+
+
+            // Handles the connectionstatechange whenever there is a combination of the states of all of the individual network transports being used by the connection
+            pc[partnerName].onconnectionstatechange = ( d ) => {
+                switch ( pc[partnerName].iceConnectionState ) {
+                    case 'disconnected':
+                    case 'failed':
+                    helper.closeVideo( partnerName );
+                        break;
+
+                    case 'closed':
+                    helper.closeVideo( partnerName );
+                        break;
+                }
+            };
+
+
+            // When the peer connection's signalingState changes, which may happen either because of a call to setLocalDescription() or to setRemoteDescription()
+            pc[partnerName].onsignalingstatechange = ( d ) => {
+                switch ( pc[partnerName].signalingState ) {
+                    case 'closed':
+                        console.log( "Signalling state is 'closed'" );
+                        helper.closeVideo( partnerName );
+                        break;
+                }
+            };
+        }
+
+         // To get the webcam's audio and video in a stream 
+         function broadcastNewTracks( stream, type, mirrorMode = true ) {
+            helper.setLocalStream( stream, mirrorMode );
  
+             let track = type == 'audio' ? stream.getAudioTracks()[0] : stream.getVideoTracks()[0];
+ 
+             for ( let p in pc ) {
+                 let pName = pc[p];
+ 
+                 if ( typeof pc[pName] == 'object' ) {
+                    helper.replaceTrack( track, pc[pName] );
+                 }
+             }
+         }
+
+         // For displaying my video on the screen
          function getAndSetUserStream() {
-             h.getUserFullMedia().then( ( stream ) => {
+            helper.getUserFullMedia().then( ( stream ) => {
                  //save my stream
                  myStream = stream;
  
-                 h.setLocalStream( stream );
+                 helper.setLocalStream( stream );
              } ).catch( ( e ) => {
                  console.error( `stream error: ${ e }` );
              } );
@@ -118,139 +245,34 @@
                  msg: msg,
                  sender: username
              };
- 
-             //emit chat message
+             // Emit chat message
              socket.emit( 'chat', data );
  
-             //add localchat
-             h.addChat( data, 'local' );
+             // Add localchat
+             helper.addChat( data, 'local' );
          }
+
+         //Chat text area
+         document.getElementById( 'chat-input' ).addEventListener( 'keypress', ( e ) => {
+            if ( e.which === 13 && ( e.target.value.trim() ) ) {
+                e.preventDefault();
+
+                sendMsg( e.target.value );
+
+                setTimeout( () => {
+                    e.target.value = '';
+                }, 50 );
+            }
+        } );
  
- 
- 
-         function init( createOffer, partnerName ) {
-             pc[partnerName] = new RTCPeerConnection( h.getIceServer() );
- 
-             if ( screen && screen.getTracks().length ) {
-                 screen.getTracks().forEach( ( track ) => {
-                     pc[partnerName].addTrack( track, screen );//should trigger negotiationneeded event
-                 } );
-             }
- 
-             else if ( myStream ) {
-                 myStream.getTracks().forEach( ( track ) => {
-                     pc[partnerName].addTrack( track, myStream );//should trigger negotiationneeded event
-                 } );
-             }
- 
-             else {
-                 h.getUserFullMedia().then( ( stream ) => {
-                     //save my stream
-                     myStream = stream;
- 
-                     stream.getTracks().forEach( ( track ) => {
-                         pc[partnerName].addTrack( track, stream );//should trigger negotiationneeded event
-                     } );
- 
-                     h.setLocalStream( stream );
-                 } ).catch( ( e ) => {
-                     console.error( `stream error: ${ e }` );
-                 } );
-             }
- 
- 
- 
-             //create offer
-             if ( createOffer ) {
-                 pc[partnerName].onnegotiationneeded = async () => {
-                     let offer = await pc[partnerName].createOffer();
- 
-                     await pc[partnerName].setLocalDescription( offer );
- 
-                     socket.emit( 'sdp', { description: pc[partnerName].localDescription, to: partnerName, sender: socketId } );
-                 };
-             }
- 
- 
- 
-             //send ice candidate to partnerNames
-             pc[partnerName].onicecandidate = ( { candidate } ) => {
-                 socket.emit( 'ice candidates', { candidate: candidate, to: partnerName, sender: socketId } );
-             };
- 
- 
- 
-             //add
-             pc[partnerName].ontrack = ( e ) => {
-                 let str = e.streams[0];
-                 if ( document.getElementById( `${ partnerName }-video` ) ) {
-                     document.getElementById( `${ partnerName }-video` ).srcObject = str;
-                 }
- 
-                 else {
-                     //video elem
-                     let newVid = document.createElement( 'video' );
-                     newVid.id = `${ partnerName }-video`;
-                     newVid.srcObject = str;
-                     newVid.autoplay = true;
-                     newVid.className = 'remote-video';
- 
-                     //video controls elements
-                     let controlDiv = document.createElement( 'div' );
-                     controlDiv.className = 'remote-video-controls';
-                     controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
-                         <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
- 
-                     //create a new div for card
-                     let cardDiv = document.createElement( 'div' );
-                     cardDiv.className = 'card card-sm';
-                     cardDiv.id = partnerName;
-                     cardDiv.appendChild( newVid );
-                     cardDiv.appendChild( controlDiv );
- 
-                     //put div in main-section elem
-                     document.getElementById( 'videos' ).appendChild( cardDiv );
- 
-                     h.adjustVideoElemSize();
-                 }
-             };
- 
- 
- 
-             pc[partnerName].onconnectionstatechange = ( d ) => {
-                 switch ( pc[partnerName].iceConnectionState ) {
-                     case 'disconnected':
-                     case 'failed':
-                         h.closeVideo( partnerName );
-                         break;
- 
-                     case 'closed':
-                         h.closeVideo( partnerName );
-                         break;
-                 }
-             };
- 
- 
- 
-             pc[partnerName].onsignalingstatechange = ( d ) => {
-                 switch ( pc[partnerName].signalingState ) {
-                     case 'closed':
-                         console.log( "Signalling state is 'closed'" );
-                         h.closeVideo( partnerName );
-                         break;
-                 }
-             };
-         }
- 
- 
- 
+         // Block for sharing screen
          function shareScreen() {
-             h.shareScreen().then( ( stream ) => {
-                 h.toggleShareIcons( true );
+            helper.shareScreen().then( ( stream ) => {
+                helper.toggleShareIcons( true );
  
                  //disable the video toggle btns while sharing screen. This is to ensure clicking on the btn does not interfere with the screen sharing
                  //It will be enabled was user stopped sharing screen
-                 h.toggleVideoBtnDisabled( true );
+                 helper.toggleVideoBtnDisabled( true );
  
                  //save my screen stream
                  screen = stream;
@@ -268,40 +290,24 @@
          }
  
  
- 
+         // BLock to stop sharing the screen
          function stopSharingScreen() {
              //enable video toggle btn
-             h.toggleVideoBtnDisabled( false );
+             helper.toggleVideoBtnDisabled( false );
  
              return new Promise( ( res, rej ) => {
                  screen.getTracks().length ? screen.getTracks().forEach( track => track.stop() ) : '';
  
                  res();
              } ).then( () => {
-                 h.toggleShareIcons( false );
+                helper.toggleShareIcons( false );
                  broadcastNewTracks( myStream, 'video' );
              } ).catch( ( e ) => {
                  console.error( e );
              } );
          }
  
- 
- 
-         function broadcastNewTracks( stream, type, mirrorMode = true ) {
-             h.setLocalStream( stream, mirrorMode );
- 
-             let track = type == 'audio' ? stream.getAudioTracks()[0] : stream.getVideoTracks()[0];
- 
-             for ( let p in pc ) {
-                 let pName = pc[p];
- 
-                 if ( typeof pc[pName] == 'object' ) {
-                     h.replaceTrack( track, pc[pName] );
-                 }
-             }
-         }
- 
- 
+         // Footer record button while screen or video is being recorded
          function toggleRecordingIcons( isRecording ) {
              let e = document.getElementById( 'record' );
  
@@ -318,7 +324,7 @@
              }
          }
  
- 
+         // Block for recording and storing the video
          function startRecording( stream ) {
              mediaRecorder = new MediaRecorder( stream, {
                  mimeType: 'video/webm;codecs=vp9'
@@ -334,7 +340,7 @@
              mediaRecorder.onstop = function () {
                  toggleRecordingIcons( false );
  
-                 h.saveRecordedStream( recordedStream, username );
+                 helper.saveRecordedStream( recordedStream, username );
  
                  setTimeout( () => {
                      recordedStream = [];
@@ -345,20 +351,6 @@
                  console.error( e );
              };
          }
- 
- 
-         //Chat textarea
-         document.getElementById( 'chat-input' ).addEventListener( 'keypress', ( e ) => {
-             if ( e.which === 13 && ( e.target.value.trim() ) ) {
-                 e.preventDefault();
- 
-                 sendMsg( e.target.value );
- 
-                 setTimeout( () => {
-                     e.target.value = '';
-                 }, 50 );
-             }
-         } );
  
  
          //When the video icon is clicked
@@ -434,7 +426,7 @@
               * Get the stream based on selection and start recording
               */
              if ( !mediaRecorder || mediaRecorder.state == 'inactive' ) {
-                 h.toggleModal( 'recording-options-modal', true );
+                helper.toggleModal( 'recording-options-modal', true );
              }
  
              else if ( mediaRecorder.state == 'paused' ) {
@@ -447,16 +439,16 @@
          } );
  
  
-         //When user choose to record the screen
+         //When user chooses to record the screen
          document.getElementById( 'record-screen' ).addEventListener( 'click', () => {
-             h.toggleModal( 'recording-options-modal', false );
+            helper.toggleModal( 'recording-options-modal', false );
  
              if ( screen && screen.getVideoTracks().length ) {
                  startRecording( screen );
              }
  
              else {
-                 h.shareScreen().then( ( screenStream ) => {
+                helper.shareScreen().then( ( screenStream ) => {
                      startRecording( screenStream );
                  } ).catch( () => { } );
              }
@@ -465,14 +457,14 @@
  
          //When user chooses to record their own video
          document.getElementById( 'record-video' ).addEventListener( 'click', () => {
-             h.toggleModal( 'recording-options-modal', false );
+            helper.toggleModal( 'recording-options-modal', false );
  
              if ( myStream && myStream.getTracks().length ) {
                  startRecording( myStream );
              }
  
              else {
-                 h.getUserFullMedia().then( ( videoStream ) => {
+                helper.getUserFullMedia().then( ( videoStream ) => {
                      startRecording( videoStream );
                  } ).catch( () => { } );
              }
