40f9587d100cb45cb25e03b4d1864c8b1138d099
nihamanchem <65664278+nihamanchem@users.noreply.github.com>
2021-07-13 12:22:13 +0530

    Update stream.js

--- src/webstream/stream.js
+++ src/webstream/stream.js
@@ -1,6 +1,6 @@
 const stream = ( socket ) => {
     socket.on( 'subscribe', ( data ) => {
-        //subscribe/join a room
+        // Subscribe the socket to a given meeting
         socket.join( data.room );
         socket.join( data.socketId );
 
@@ -10,22 +10,21 @@ const stream = ( socket ) => {
         }
     } );
 
-
     socket.on( 'newUserStart', ( data ) => {
         socket.to( data.to ).emit( 'newUserStart', { sender: data.sender } );
     } );
 
-
+    // Upon recieving sdp info regarding media transfer endpoint, the server will re-emit the description to all the sockets
     socket.on( 'sdp', ( data ) => {
         socket.to( data.to ).emit( 'sdp', { description: data.description, sender: data.sender } );
     } );
 
-
+    // Upon recieving info of the ice candidates from the STUN server, the data is again transmitted to all the sockets
     socket.on( 'ice candidates', ( data ) => {
         socket.to( data.to ).emit( 'ice candidates', { candidate: data.candidate, sender: data.sender } );
     } );
 
-
+    // Chat connection 
     socket.on( 'chat', ( data ) => {
         socket.to( data.room ).emit( 'chat', { sender: data.sender, msg: data.msg } );
     } );
