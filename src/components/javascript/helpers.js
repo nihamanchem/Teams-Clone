28a1a2e4cec8ac55de3e81b2f31bbc2aeda620c8
nihamanchem <65664278+nihamanchem@users.noreply.github.com>
2021-07-13 12:21:24 +0530

    Update helpers.js

--- src/components/javascript/helpers.js
+++ src/components/javascript/helpers.js
@@ -1,4 +1,6 @@
 export default {
+
+    // Helper to create meeting ID
     generateRandomString() {
         const crypto = window.crypto || window.msCrypto;
         let array = new Uint32Array(1);
@@ -7,18 +9,28 @@ export default {
     },
 
 
-    closeVideo( elemId ) {
-        if ( document.getElementById( elemId ) ) {
-            document.getElementById( elemId ).remove();
-            this.adjustVideoElemSize();
-        }
+    // Connection with XIRSYS service which gives access to its TURN server
+    getIceServer() {
+        return {
+            iceServers: [
+                {
+                    urls: ["stun:bn-turn1.xirsys.com"]
+                },
+                {
+                    username: "ER0corTKBAJhRBduZwLvkaRZ2gEwOvRPDBeZoc5K0VWq44fpc9i8Fwf9L4wd7VPXAAAAAGDkc39uaWhhbWFuY2hlbQ==",   
+                    credential: "f5542c4c-de6c-11eb-9b5e-0242ac140004",  
+                    urls: [
+                        "turn:bn-turn1.xirsys.com:80?transport=udp",
+                        "turn:bn-turn1.xirsys.com:3478?transport=udp",
+                        "turn:bn-turn1.xirsys.com:80?transport=tcp",       
+                        "turn:bn-turn1.xirsys.com:3478?transport=tcp",       
+                        "turns:bn-turn1.xirsys.com:443?transport=tcp",
+                        "turns:bn-turn1.xirsys.com:5349?transport=tcp" 
+                    ]
+                }
+            ]
+        };
     },
-
-
-    pageHasFocus() {
-        return !( document.hidden || document.onfocusout || window.onpagehide || window.onblur );
-    },
-
 
     getQString( url = '', keyToReturn = '' ) {
         url = url ? url : location.href;
@@ -48,11 +60,13 @@ export default {
     },
 
 
+    // Helper to check if the user media is available or not
     userMediaAvailable() {
         return !!( navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia );
     },
 
 
+    // Helper for both video and audio
     getUserFullMedia() {
         if ( this.userMediaAvailable() ) {
             return navigator.mediaDevices.getUserMedia( {
@@ -63,13 +77,13 @@ export default {
                 }
             } );
         }
-
+    
         else {
             throw new Error( 'User media not available' );
         }
     },
-
-
+    
+    // Helper for audio
     getUserAudio() {
         if ( this.userMediaAvailable() ) {
             return navigator.mediaDevices.getUserMedia( {
@@ -79,14 +93,27 @@ export default {
                 }
             } );
         }
-
+    
         else {
             throw new Error( 'User media not available' );
         }
     },
 
+    // Helper when one of audience exits the meeting 
+    closeVideo( elemId ) {
+        if ( document.getElementById( elemId ) ) {
+            document.getElementById( elemId ).remove(); // removes the participant
+            this.adjustVideoElemSize(); // adjusts other participant's video window
+        }
+    },
 
 
+    // Helper to stop access to footer icons (UI)
+    toggleVideoBtnDisabled( disabled ) {
+        document.getElementById( 'toggle-video' ).disabled = disabled;
+    },
+
+    // Helper to share screen
     shareScreen() {
         if ( this.userMediaAvailable() ) {
             return navigator.mediaDevices.getDisplayMedia( {
@@ -107,28 +134,10 @@ export default {
     },
 
 
-    getIceServer() {
-        return {
-            iceServers: [
-                {
-                    urls: ["stun:bn-turn1.xirsys.com"]
-                },
-                {
-                    username: "ER0corTKBAJhRBduZwLvkaRZ2gEwOvRPDBeZoc5K0VWq44fpc9i8Fwf9L4wd7VPXAAAAAGDkc39uaWhhbWFuY2hlbQ==",   
-                    credential: "f5542c4c-de6c-11eb-9b5e-0242ac140004",  
-                    urls: [
-                        "turn:bn-turn1.xirsys.com:80?transport=udp",
-                        "turn:bn-turn1.xirsys.com:3478?transport=udp",
-                        "turn:bn-turn1.xirsys.com:80?transport=tcp",       
-                        "turn:bn-turn1.xirsys.com:3478?transport=tcp",       
-                        "turns:bn-turn1.xirsys.com:443?transport=tcp",
-                        "turns:bn-turn1.xirsys.com:5349?transport=tcp" 
-                    ]
-                }
-            ]
-        };
-    },
 
+    pageHasFocus() {
+        return !( document.hidden || document.onfocusout || window.onpagehide || window.onblur );
+    },
 
     addChat( data, senderType ) {
         let chatMsgDiv = document.querySelector( '#chat-messages' );
@@ -172,6 +181,7 @@ export default {
     },
 
 
+    // When new chat message is recieved
     toggleChatNotificationBadge() {
         if ( document.querySelector( '#chat-pane' ).classList.contains( 'chat-opened' ) ) {
             document.querySelector( '#new-chat-notification' ).setAttribute( 'hidden', true );
@@ -183,7 +193,7 @@ export default {
     },
 
 
-
+    // To replace the track currently being used as the sender's source with a new MediaStreamTrack
     replaceTrack( stream, recipientPeer ) {
         let sender = recipientPeer.getSenders ? recipientPeer.getSenders().find( s => s.track && s.track.kind === stream.kind ) : false;
 
@@ -191,7 +201,7 @@ export default {
     },
 
 
-
+    // Helper for sharing the screen
     toggleShareIcons( share ) {
         let shareIconElem = document.querySelector( '#share-screen' );
 
@@ -209,11 +219,7 @@ export default {
     },
 
 
-    toggleVideoBtnDisabled( disabled ) {
-        document.getElementById( 'toggle-video' ).disabled = disabled;
-    },
-
-
+    // Helper to full screen a particular audience
     maximiseStream( e ) {
         let elem = e.target.parentElement.previousElementSibling;
 
@@ -221,6 +227,7 @@ export default {
     },
 
 
+    // Helper to mute a particular audience
     singleStreamToggleMute( e ) {
         if ( e.target.classList.contains( 'fa-microphone' ) ) {
             e.target.parentElement.previousElementSibling.muted = true;
@@ -236,6 +243,7 @@ export default {
     },
 
 
+    // Helper to save the recording on your device
     saveRecordedStream( stream, user ) {
         let blob = new Blob( stream, { type: 'video/webm' } );
 
@@ -245,22 +253,23 @@ export default {
     },
 
 
+    // Helper to display the record screen
     toggleModal( id, show ) {
-        let el = document.getElementById( id );
+        let elem = document.getElementById( id );
 
         if ( show ) {
-            el.style.display = 'block';
-            el.removeAttribute( 'aria-hidden' );
+            elem.style.display = 'block';
+            elem.removeAttribute( 'aria-hidden' );
         }
 
         else {
-            el.style.display = 'none';
-            el.setAttribute( 'aria-hidden', true );
+            elem.style.display = 'none';
+            elem.setAttribute( 'aria-hidden', true );
         }
     },
 
 
-
+    // Helper to get the webcam's audio and video in a stream 
     setLocalStream( stream, mirrorMode = true ) {
         const localVidElem = document.getElementById( 'local' );
 
@@ -269,6 +278,7 @@ export default {
     },
 
 
+    // To adjust the attendees' video as new attendees join in
     adjustVideoElemSize() {
         let elem = document.getElementsByClassName( 'card' );
         let totalRemoteVideosDesktop = elem.length;
@@ -303,20 +313,20 @@ export default {
             newVid.autoplay = true;
             newVid.className = 'remote-video';
 
-            //video controls elements
+            // Video control elements
             let controlDiv = document.createElement( 'div' );
             controlDiv.className = 'remote-video-controls';
             controlDiv.innerHTML = `<i class="fa fa-microphone text-white pr-3 mute-remote-mic" title="Mute"></i>
                 <i class="fa fa-expand text-white expand-remote-video" title="Expand"></i>`;
 
-            //create a new div for card
+            // Creates a new div for card
             let cardDiv = document.createElement( 'div' );
             cardDiv.className = 'card card-sm';
             cardDiv.id = `demo-${ i }`;
             cardDiv.appendChild( newVid );
             cardDiv.appendChild( controlDiv );
 
-            //put div in main-section elem
+            // Put div in main-section elem
             document.getElementById( 'videos' ).appendChild( cardDiv );
 
             this.adjustVideoElemSize();
